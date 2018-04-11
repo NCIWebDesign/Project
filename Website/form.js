a2dd3ae00eb2aa7bd640dd0f9380aed57b99eeb5
DaveC42 <36533038+DaveC42@users.noreply.github.com>
2018-04-11 17:53:30 +0100

    Update form.js

--- Website/form.js
+++ Website/form.js
@@ -36,18 +36,19 @@ var a ; if (confirm("Press a button!")
 else { a = "You pressed Cancel!"; } 
 document.getElementById("demo").innerHTML= x; } 
 
-function initMap() {
-        var Waterford, Ireland = {lat: 52° 05' 37" N, lang: -7° 59' 08" W};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          right: =Waterford
-        });
-        var marker = new google.maps.Marker({
-          position: Waterford,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
-    </script>
+<script>
+  function initMap() {
+    var Waterford = {lat: 52.259311, lng: -7.110343};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: Waterford
+    });
+    var marker = new google.maps.Marker({
+      position: Waterford,
+      map: map
+    });
+  }
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2ioL-76_dxSNYGxV-FExa_lAiFlLCpp0&callback=initMap">
+</script>
